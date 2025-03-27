b6d2ec433863c250468deee0a1bc329aae43386a
Selene <selenedanyluk@gmail.com>
2025-03-27 11:08:21 -0300

    add newBook component

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,30 @@
+import { useState } from "react";
 import "./App.css";
-import BookItem from "./components/bookItem/BookItem";
-import books from "./data/books";
+import Books from "./components/books/Books";
+import NewBook from "./components/newBook/NewBook";
+import booksInitial from "./data/books";
 
 function App() {
+
+  const [books, setBooks] = useState(booksInitial);
+  const handleBookAdded = (enteredBook) => {
+    console.log(enteredBook);
+    const bookData = {
+      ...enteredBook,
+      id: Math.random().toString(),
+      bookRating: Array(enteredBook.rating).fill("*"),  
+    };
+    setBooks((prevBooks) => {
+      return [...prevBooks, bookData];
+    });
+  };
+
   return (
     <>
       <h2>Book champions app</h2>
       <p>¡Quiero leer libros!</p>
-      {books.map((book, index) => (
-        <BookItem
-          key={index}
-          bookTitle={book.bookTitle}
-          author={book.bookAuthor}
-          rating={book.bookRating}
-          pages={book.pageCount}
-          imageUrl={book.imageUrl}
-        />
-      ))}
+      <NewBook onBookAdded={handleBookAdded} />
+      <Books books={books}/>
     </>
   );
 }
